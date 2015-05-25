77192dadc8fc960d3d809b443054e22a8f5ae956
ayf@hp <ayf@hp>
2015-05-25 17:50:01 -0400

    autoupdate 2015-05-25-17:50:01

--- thesis.R
+++ thesis.R
@@ -7,6 +7,7 @@ library(methods)
 library(xtable)
 library(knitr)
 library(dunn.test)
+library(pander)
 
 options(bitmapType="cairo")
 
@@ -1230,6 +1231,6 @@ mydata <- read.csv(file.path(datadir, "2012.08.23.7daysTD.csv"), header = FALSE,
 mycolnames <- read.table(file.path(datadir, "2012.08.23.7daysTD.csv"), header = FALSE, sep = ",", nrows = 1)
 for (i in 4:length(mycolnames)) {
   print(mycolnames[i])
-  print(aggregate(mydata[,i], list(mydata$V2), mean, na.rm = TRUE))
+  pandoc.table(aggregate(mydata[,i], list(mydata$V2), mean, na.rm = TRUE))
   dunn.test(mydata[,i], mydata$V2, method = "bonferroni")
 }
