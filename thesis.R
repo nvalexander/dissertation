d197c87d46f55b71b5a7738adac935e597ff23a0
ayf@hp <ayf@hp>
2015-05-25 22:40:01 -0400

    autoupdate 2015-05-25-22:40:01

--- thesis.R
+++ thesis.R
@@ -20,16 +20,16 @@ codedirpath <- dirname(
 datadir <- normalizePath(file.path(codedirpath, "data"))
 invivodataonedays <- read.csv(file.path(datadir, "2012.12.09.1dayTD.csv"), header = TRUE)
 invivocolnameonedays <- read.table(file.path(datadir, "2012.12.09.1dayTD.csv"), header = FALSE, sep = ",", nrows = 1)
-levels(invivodataonedays$V2)[levels(invivodataonedays$V2)=="A"] <- "C"
-levels(invivodataonedays$V2)[levels(invivodataonedays$V2)=="E"] <- "D"
-levels(invivodataonedays$V2)[levels(invivodataonedays$V2)=="W"] <- "V"
-levels(invivodataonedays$V2)[levels(invivodataonedays$V2)=="S"] <- "T"
+levels(invivodataonedays$treatment)[levels(invivodataonedays$treatment)=="A"] <- "C"
+levels(invivodataonedays$treatment)[levels(invivodataonedays$treatment)=="E"] <- "D"
+levels(invivodataonedays$treatment)[levels(invivodataonedays$treatment)=="W"] <- "V"
+levels(invivodataonedays$treatment)[levels(invivodataonedays$treatment)=="S"] <- "T"
 invivodatathreedays <- read.csv(file.path(datadir, "2012.12.12.3daysTD.csv"), header = TRUE)
 invivocolnamethreedays <- read.table(file.path(datadir, "2012.12.12.3daysTD.csv"), header = FALSE, sep = ",", nrows = 1)
-levels(invivodatathreedays$V2)[levels(invivodatathreedays$V2)=="B"] <- "C"
-levels(invivodatathreedays$V2)[levels(invivodatathreedays$V2)=="G"] <- "D"
-levels(invivodatathreedays$V2)[levels(invivodatathreedays$V2)=="X"] <- "V"
-levels(invivodatathreedays$V2)[levels(invivodatathreedays$V2)=="U"] <- "T"
+levels(invivodatathreedays$treatment)[levels(invivodatathreedays$treatment)=="B"] <- "C"
+levels(invivodatathreedays$treatment)[levels(invivodatathreedays$treatment)=="G"] <- "D"
+levels(invivodatathreedays$treatment)[levels(invivodatathreedays$treatment)=="X"] <- "V"
+levels(invivodatathreedays$treatment)[levels(invivodatathreedays$treatment)=="U"] <- "T"
 invivodatasevendays <- read.csv(file.path(datadir, "2012.08.23.7daysTD.csv"), header = TRUE)
 invivocolnamesevendays <- read.table(file.path(datadir, "2012.08.23.7daysTD.csv"), header = FALSE, sep = ",", nrows = 1)
 
@@ -1261,18 +1261,18 @@ reportstats <- function(invivodata, invivocolnames){
     myoutput <- paste0(myoutput, 
                        "# ", invivocolnames[[i]])
     myoutput <- paste(myoutput, 
-                      pandoc.table.return(aggregate(invivodata[,i], list(invivodata$V2), mean, na.rm = TRUE), style = "rmarkdown"))
-    kw <- kruskal.test(as.formula(paste(colnames(invivocolnames)[i], "~V2")), data = invivodata)
+                      pandoc.table.return(aggregate(invivodata[,i], list(invivodata$treatment), mean, na.rm = TRUE), style = "rmarkdown"))
+    kw <- kruskal.test(as.formula(paste(colnames(invivodata)[i], "~treatment")), data = invivodata)
     myoutput <- paste(myoutput, 
                       paste0("Kruskal-Wallis p value for the four-way comparison is ", signif(kw$p.value, digits = 3)))
-    dunns <- dunn.test(invivodata[,i], invivodata$V2, method = "bonferroni")
+    dunns <- dunn.test(invivodata[,i], invivodata$treatment, method = "bonferroni")
     dunnsreport <- data.frame(contrastsfour, dunns$P)
     myoutput <- paste(myoutput,
                       pandoc.table.return(dunnsreport, style = "rmarkdown"))
-    kw <- kruskal.test(as.formula(paste(colnames(invivodatasubset)[i], "~V2")), data = invivodatasubset)
+    kw <- kruskal.test(as.formula(paste(colnames(invivodata)[i], "~treatment")), data = invivodatasubset)
     myoutput <- paste(myoutput, 
                       paste0("Kruskal-Wallis p value for the three-way comparison is ", signif(kw$p.value, digits = 3)))
-    dunns <- dunn.test(invivodatasubset[,i], invivodatasubset$V2, method = "bonferroni")
+    dunns <- dunn.test(invivodatasubset[,i], invivodatasubset$treatment, method = "bonferroni")
     dunnsreport <- data.frame(contraststhree, dunns$P)
     myoutput <- paste(myoutput,
                       pandoc.table.return(dunnsreport, style = "rmarkdown"))
