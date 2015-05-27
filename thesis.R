8212f6399b7645852a0d8d41e31227a7a9bd8244
ayf@hp <ayf@hp>
2015-05-26 22:20:02 -0400

    autoupdate 2015-05-26-22:20:02

--- thesis.R
+++ thesis.R
@@ -38,7 +38,7 @@ DvsCthreeways <- match("D vs DT", contraststhree)[[1]]
 VvsCthreeways <- match("V vs DT", contraststhree)[[1]]
 conditionsVDC <- c("Veh", "Dexa", "Comb")
 conditionsVDTC <- c("Veh", "Dexa", "Testo", "Comb")
-unistar <- sprintf('\u2600')
+unistar <- sprintf('\u00D7')
 unidagger <- sprintf('\u2020')
 
 datadir <- normalizePath(file.path(codedirpath, "data"))
@@ -210,7 +210,7 @@ plotbodyweightcourse <- function(){
   shortdf <- invivodatasubsetsevendays[, colnames(invivodatasubsetsevendays) %in% c("treatment", timeseriescolumns)]
   body.weight.gain.after.0.days..percent. <- rep(0, dim(shortdf)[1])
   statsstars <- c("", "", "",
-                  "", paste0(unidagger, unistar, "*"),"",
+                  "", paste0(unidagger, " ", unistar),"",
                   "", unidagger,"",
                   "", unidagger,"",
                   "", unidagger,"",
@@ -235,7 +235,8 @@ plotbodyweightcourse <- function(){
                  fun.y = mean, 
                  position = position_dodge(.05)) + 
     stat_summary(geom = "text", 
-                 size = textSize * .4, 
+                 size = textSize * .4,
+                 aes(family = "serif"),
                  fun.y = belowSEM, 
                  hjust = -.2,
                  vjust = .25,
