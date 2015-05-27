876b249ad3b159cb60ebe554744d471eca2e99ae
ayf@hp <ayf@hp>
2015-05-27 00:40:01 -0400

    autoupdate 2015-05-27-00:40:01

--- setup4lyx.sh
+++ setup4lyx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MY_DIR=$(dirname $(readlink -f $0))
 ln -f -s ${MY_DIR}/pwl_english.dict  ${HOME}/.lyx/pwl_english.dict
+cp /usr/local/share/lyx/ui/stdtoolbars.inc  ~/.lyx/ui/stdtoolbars.inc
 #sudo texlua install-getnonfreefonts
 #sudo /media/ayf/store/opt/texlive2014/texmf-dist/scripts/getnonfreefonts/getnonfreefonts.pl -a
 #sudo /media/ayf/store/opt/texlive2014/texmf-dist/scripts/getnonfreefonts/getnonfreefonts.pl -a
