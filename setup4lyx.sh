1af3e020fa39c0dbadc68a697d47219d916c18ff
ayf@hp <ayf@hp>
2015-06-02 19:40:03 -0400

    autoupdate 2015-06-02-19:40:03

--- setup4lyx.sh
+++ setup4lyx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 MY_DIR=$(dirname $(readlink -f $0))
-ln -f -s ${MY_DIR}/pwl_english.dict  ${HOME}/.lyx/pwl_english.dict
-cp /usr/local/share/lyx/ui/stdtoolbars.inc  ~/.lyx/ui/stdtoolbars.inc
+ln -f -s ${MY_DIR}/custom.dic ${HOME}/.lyx/pwl_english.dict
+ln -f -s ${MY_DIR}/custom.dic ${HOME}/.config/enchant/en_US.dic
+ln -f -s ${MY_DIR}/stdtoolbars.inc ${HOME}/.lyx/ui/stdtoolbars.inc
 #sudo texlua install-getnonfreefonts
 #sudo /media/ayf/store/opt/texlive2014/texmf-dist/scripts/getnonfreefonts/getnonfreefonts.pl -a
 #sudo /media/ayf/store/opt/texlive2014/texmf-dist/scripts/getnonfreefonts/getnonfreefonts.pl -a
